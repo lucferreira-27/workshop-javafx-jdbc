dc54b139bd54079721bec0deecac237b0a77e6e2
Lucas Ferreira <lucferreira14@gmail.com>
2020-04-06 22:07:05 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,6 +11,8 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
 	@Override
 	public void start(Stage stage) {
 		try {
@@ -22,7 +24,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			 mainScene = new Scene(scrollPane);
 			stage.setScene(mainScene);
 			stage.setTitle("Sample JavaFX application");
 			
@@ -33,7 +35,9 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
 	public static void main(String[] args) {
 		launch(args);
 	}
